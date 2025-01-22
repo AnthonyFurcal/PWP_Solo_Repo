e79a558922a384fff44eac0f1a76ee7b7ec7a0b1
Anthony Furcal <149692624+AnthonyFurcal@users.noreply.github.com>
2025-01-22 08:42:09 -0500

    Update main.py

--- CircleDetect/main.py
+++ CircleDetect/main.py
@@ -1,4 +1,4 @@
-# THIS IS ANTHONY FURCAL'S PROGRAMMING PROJECT FOR THE PARALLEL LINE AND FINDING CENTER LINE WINTER BREAK ASSIGNMENT
+# THIS IS ANTHONY FURCAL'S PROGRAMMING PROJECT FOR THE CIRCLE DETECTION ASSIGNMENT
 
 
 from types import NoneType
@@ -6,33 +6,34 @@ from types import NoneType
 import cv2
 import numpy as np
 
-img = cv2.imread('SodaPhotos/Can.jpg')
-kernel = np.ones((255,255),np.uint8)
+img = cv2.imread('SodaPhotos/Can2.jpg')
+kernel = np.ones((255, 255), np.uint8)
 
 
 def stream_processing():
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        # Thresholding the image to highlight lines (binary image)
-        _, thresh = cv2.threshold(gray, 123, 195, cv2.THRESH_BINARY_INV)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
 
-        closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+    # Thresholding the image to highlight lines (binary image)
+    _, thresh = cv2.threshold(blur, 145, 255, cv2.THRESH_BINARY)
+    masked_image = cv2.bitwise_and(blur, blur, mask=thresh)
 
-        # Blurring the image to reduce noise that makes it more difficult to process
+    # Blurring the image to reduce noise that makes it more difficult to process
 
-        blur = cv2.GaussianBlur(closed, (7, 7), 5)
+    circles = cv2.HoughCircles(masked_image, cv2.HOUGH_GRADIENT, 1, 255,
+                               param1=100, param2=70, minRadius=50, maxRadius=0)
 
-        circles = cv2.HoughCircles(blur, cv2.HOUGH_GRADIENT, 1, 100,
-                                  param1=100, param2=70, minRadius=50, maxRadius=0)
+    try:
 
         circles = np.uint16(np.around(circles))
         for i in circles[0, :]:
             # draw the outer circle
-            cv2.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 20)
+            cv2.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 10)
             # draw the center of the circle
-            cv2.circle(img, (i[0], i[1]), 2, (0, 0, 255), 20)
+            cv2.circle(img, (i[0], i[1]), 2, (0, 0, 255), 10)
 
-                # Create a named window
+        # Create a named window
         cv2.namedWindow('Photo', cv2.WINDOW_NORMAL)
 
         # Resize the window
@@ -47,7 +48,22 @@ def stream_processing():
             # Closing the Window
             cv2.destroyAllWindows()
 
+    except TypeError:
 
+        cv2.namedWindow('Photo', cv2.WINDOW_NORMAL)
+
+        # Resize the window
+        cv2.resizeWindow('Photo', 800, 600)
+
+        cv2.imshow('Photo', thresh)
+        cv2.waitKey(0)
+
+        # Inputting q on the keyboard ends the program
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            # Closing the Window
+            cv2.destroyAllWindows()
+        print("No Circles Found")
 
 
 stream_processing()
